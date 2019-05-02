f79f706bf8d66dc5013cfd993b6b9d4fbdf54a78
Abdullah Al Mamun <almamunabdullah647@gmail.com>
2019-05-02 12:01:54 +0600

    IELTS Assistance

--- app/src/main/java/com/example/mamun/ieltsassistance/MainActivity.java
+++ app/src/main/java/com/example/mamun/ieltsassistance/MainActivity.java
@@ -3,24 +3,35 @@ package com.example.mamun.ieltsassistance;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 ImageView listening1,reading1,writing,speaking;
 Button author1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 author1=findViewById(R.id.author);
+author1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+      //  startActivity(new Intent(MainActivity.this,pop.class));
+        Intent intent=new Intent(MainActivity.this,author.class);
+        startActivity(intent);
+    }
+});
 
     }
-
-
 
 
 
