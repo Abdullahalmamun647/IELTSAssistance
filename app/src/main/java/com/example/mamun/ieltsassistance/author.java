07afe332d57f7c8bbdbebdd7dbc237a7b54c0431
Abdullah Al Mamun <almamunabdullah647@gmail.com>
2019-05-19 10:49:37 +0600

    new19-5-2019

--- app/src/main/java/com/example/mamun/ieltsassistance/author.java
+++ app/src/main/java/com/example/mamun/ieltsassistance/author.java
@@ -1,8 +1,11 @@
 package com.example.mamun.ieltsassistance;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,8 +15,23 @@ Button face;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author);
-        face=findViewById(R.id.facebook);
+
         face.setMovementMethod(LinkMovementMethod.getInstance());
 
+    }
+
+    public void openBrowser(View view){
+
+        //Get url from tag
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse("https://www.facebook.com/mamunyourboss"));
+
+        startActivity(intent);
     }
 }
